2908e603f8af7022721bf4696a073259e0b270e2
Dmitry <dmitronus2015@gmail.com>
2025-01-20 17:57:41 +0200

    add strategies adaptive js

--- js/main.js
+++ js/main.js
@@ -5,5 +5,23 @@ $(function () {
     dots: true,
     arrows: false,
     initialSlide: 3,
+    responsive: [
+      {
+        breakpoint: 1400,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 4,
+        },
+      },
+      {
+        breakpoint: 865,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 4,
+        },
+      },
+    ],
   });
 });
